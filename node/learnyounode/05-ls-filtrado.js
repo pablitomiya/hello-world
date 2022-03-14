feff04d4b5730c8625c4acf5aa73f0d8e24fdb29
pablitomiya <pablitomiya@gmail.com>
2022-03-14 20:03:31 -0300

    frontend

--- node/learnyounode/05-ls-filtrado.js
+++ node/learnyounode/05-ls-filtrado.js
@@ -1,15 +1,17 @@
-const fs = require('fs')
-const path = require('path')
-const dir = process.argv[2]
-const ext = process.argv[3]
-fs.readdir(dir,(err,arrFiles)=>{
-    if (err){return console.log(err)}
-    // console.log(arrFiles)
-    arrFiles.forEach((elemento)=>{
-        const arrFiltered = path.extname(elemento)
-        if(arrFiltered==`.${ext}`){
-            console.log(elemento)
-        }
-    })
-    // console.log(arTexto)
-})
+const fs = require("fs");
+const path = require("path");
+const dir = process.argv[2];
+const ext = process.argv[3];
+fs.readdir(dir, (err, arrFiles) => {
+  if (err) {
+    return console.log(err);
+  }
+  // console.log(arrFiles)
+  arrFiles.forEach((elemento) => {
+    const arrFiltered = path.extname(elemento);
+    if (arrFiltered == `.${ext}`) {
+      console.log(elemento);
+    }
+  });
+  // console.log(arTexto)
+});
